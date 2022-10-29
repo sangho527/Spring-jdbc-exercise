c184e0d8ff6640b3e63246683b53e749208d8047
sangho527 <sy980527@naver.com>
2022-10-29 23:58:08 +0900

    UserDao 중간 커밋 221029

--- src/main/java/com/dao/UserDao.java
+++ src/main/java/com/dao/UserDao.java
@@ -1,22 +1,23 @@
 package com.dao;
 
-import com.domain.User;
+import com.connection.ConnectionMaker;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
 import java.util.List;
-import java.util.Map;
 
 public class UserDao {
+    ConnectionMaker connectionMaker;
+    Connection connection;
     private JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void add(User user){
+    public void add(UserDaoFactory.User user){
         this.jdbcTemplate.update("insert into users (id, name, pasword) values (?,?,?);",
                 user.getId(), user.getName(), user.getPassword());
     }
@@ -29,24 +30,24 @@ public class UserDao {
         return this.jdbcTemplate.queryForObject("select count)*)from users;",Integer.class);
     }
 
-    public User findById(String id) {
+    public UserDaoFactory.User findById(String id) {
         String sql = "select *from users where id = ?";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
+        RowMapper<UserDaoFactory.User> rowMapper = new RowMapper<UserDaoFactory.User>() {
             @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
+            public UserDaoFactory.User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                UserDaoFactory.User user = new UserDaoFactory.User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
                 return user;
             }
         };
         return this.jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public List<User> getAll(){
+    public List<UserDaoFactory.User> getAll(){
         String sql = "select *from users order by id";
-        RowMapper<User> rowMapper = new RowMapper<User>() {
+        RowMapper<UserDaoFactory.User> rowMapper = new RowMapper<UserDaoFactory.User>() {
             @Override
-            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                User user = new User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
+            public UserDaoFactory.User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                UserDaoFactory.User user = new UserDaoFactory.User(rs.getString("id"),rs.getString("name"),rs.getString("password"));
                 return user;
             }
         };
